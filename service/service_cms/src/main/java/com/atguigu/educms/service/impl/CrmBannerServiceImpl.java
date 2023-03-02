27cf11a19b2dbe4b8c9e259e01da0a7de8dd2cf9
eniac555 <1150304664@qq.com>
2023-03-02 16:42:13 +0800

    first commit

--- service/service_cms/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
+++ service/service_cms/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
@@ -3,8 +3,12 @@ package com.atguigu.educms.service.impl;
 import com.atguigu.educms.entity.CrmBanner;
 import com.atguigu.educms.mapper.CrmBannerMapper;
 import com.atguigu.educms.service.CrmBannerService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
 
+    //查询所有banner
+    @Cacheable(value = "banner",key = "'selectIndexList'")
+    @Override
+    public List<CrmBanner> selectAllBanner() {
+
+        //新增：根据id降序排列，显示排列后的前两条记录
+        QueryWrapper<CrmBanner> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("id");
+        //last方法，拼接
+        wrapper.last("limit 2");
+
+
+
+        baseMapper.selectList(null);
+        return null;
+    }
 }
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/IndexFrontController.java
@@ -0,0 +1,49 @@
+package com.atguigu.eduservice.controller.front;
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduCourseService;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/eduservice/indexfront")
+public class IndexFrontController {
+    //查询热门课程和名师的接口
+
+    @Autowired
+    private EduCourseService courseService;
+
+    @Autowired
+    private EduTeacherService teacherService;
+
+
+    //查询前八条热门课程   查询前四个名师
+    @GetMapping("index")
+    public R index(){
+        //查询前八条热门课程
+        QueryWrapper<EduCourse> wrapper1 = new QueryWrapper<>();
+        wrapper1.orderByDesc("id");
+        wrapper1.last("limit 8");
+        List<EduCourse> eduList = courseService.list(wrapper1);
+
+        //查询前四个名师
+        QueryWrapper<EduTeacher> wrapper2 = new QueryWrapper<>();
+        wrapper2.orderByAsc("id");
+        wrapper2.last("limit 4");
+        List<EduTeacher> eduTeacherList = teacherService.list(wrapper2);
+
+        //返回结果
+        return R.ok().data("eduList",eduList).data("eduTeacherList",eduTeacherList);
+    }
+
+}
